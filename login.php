a3db77936cdc9a68f1a75a6b7229b7362c9ef10b
Unknown <josephkeller@suumail.net>
2018-11-12 17:17:26 -0700

    gitflow-feature-stash: LoginPageWithRedirect

--- login.php
+++ login.php
@@ -1,25 +1,36 @@
+<?php
+
+session_start();
+
+echo "
 <!DOCTYPE html>
 
 <html>
 <head>
+<script src='http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js' type='text/javascript'></script>
 <script>
 	function alertTest(){
 		
-		alert(document.getElementById("userText").value);
-		alert(document.getElementById("passText").value);
+		alert(document.getElementById('userText').value);
+		alert(document.getElementById('passText').value);
 		
 	}
 	
 	function setCookie(){
-		document.cookie = "username=" + document.getElementById("userText").value;
-		document.cookie = "password=" + document.getElementById("passText").value;
-		alert(document.cookie);
+		document.cookie = 'username=' + document.getElementById('userText').value;
+		document.cookie = 'password=' + document.getElementById('passText').value;
+		
 	}
 	
-	function getCookie(){
-		var cookiesArray = document.cookie.split("; ");
-		alert(cookiesArray[0]);
-		alert(cookiesArray[1]);
+	function checkLogin(){
+		var cookiesArray = document.cookie.split('; ');
+		var firstTxtArray= cookiesArray[0].split('=');
+		var secondTxtArray= cookiesArray[1].split('=');
+		var testData = {
+			un: firstTxtArray[1],
+			pw: secondTxtArray[1]
+		};
+		$.post('checkLogin.php',testData);
 	}
 </script>
 <style>
@@ -48,7 +59,7 @@
 
 
 
-.loginbox input[type="text"], input[type="password"]
+.loginbox input[type='text'], input[type='password']
 {
 	width: 100%;
 	margin-bottom: 5px;
@@ -62,7 +73,7 @@
 	
 }
 
-.loginbox input[type="submit"]
+.loginbox input[type='submit']
 {
 	border: none;
 	outline: none;
@@ -74,7 +85,7 @@
 	
 }
 
-.loginbox input[type="submit"]:hover{
+.loginbox input[type='submit']:hover{
 	cursor: pointer;
 }
 
@@ -92,16 +103,46 @@
 
 </style>
 
-<body style="background-color:darkred" onload="getCookie()">
-<div class="loginbox">
-<form method="get">
+<body style='background-color:darkred'>
+<div class='loginbox'>
+<form action= 'login.php' method='post'>
   
-  <input type="text" placeholder="Enter Username" name="username" id="userText">
-  <input type="password" placeholder="Enter Password" name="password" id="passText">
-  <input type="submit" name="" value="Login" id="loginTxt" onclick="setCookie()">
-  <a href="#"> Forgot password?</a><br>
+  <input type='text' placeholder='Enter Username' name='username' id='userText'>
+  <input type='password' placeholder='Enter Password' name='password' id='passText'>
+  <input type='submit' name='' value='Login' id='loginTxt'>
+  <a href='#'> Forgot password?</a><br>
 </form>
 </div>
 </body>
 </head>
 </html>
+";
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+	$username = $_POST["username"];
+	$password = $_POST["password"];
+	
+	$dbh = new PDO('sqlite:CHSxlt.db') or die("cannot connect to database");
+					$sql = "SELECT Pword FROM User WHERE Uname = '".$username."'";
+					$query = $dbh->query($sql);
+					$result = $query->fetch(PDO::FETCH_ASSOC);
+					$loginOkay =1;
+					foreach($result as $res){
+						$loginOkay=strcasecmp($res,$password);
+					
+					}
+					
+					if($loginOkay==0){
+						header("Location: index.php");
+					}
+					else{
+						echo "<script> alert('The username/password are incorrect');</script>";
+					}
+	
+	
+	
+	
+	
+}
+	
+?>
